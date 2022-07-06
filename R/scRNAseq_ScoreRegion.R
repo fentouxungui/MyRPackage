a031b2b54ce04cef6c54f7d038fd06e33a2dde6e
fentouuxngui <zhangyongchao@nibs.ac.cn>
2022-07-06 15:22:43 +0800

    支持自定义Regional Markers

--- R/scRNAseq_ScoreRegion.R
+++ R/scRNAseq_ScoreRegion.R
@@ -4,6 +4,7 @@
 #' @param Meta A data frame with new name and old name
 #' @param from old name
 #' @param to new name
+#' @param output.lost.genes output the genes exist in RNA-seq data while not exist in scRNA-seq data, Default FALSE.
 #'
 #' @return A data frame with row names changed (rows may not as long as before)
 #' @export
@@ -16,10 +17,15 @@
 scRNAseq_Score_Region_Check <- function(BulkRNAseq.expr,
                                         Meta,
                                         from = "gene_id",
-                                        to = "gene_name"){
+                                        to = "gene_name",
+                                        output.lost.genes = FALSE){
   # how many row names of BulkRNAseq.expr not exist in Meta.
   not.exists <- rownames(BulkRNAseq.expr)[!rownames(BulkRNAseq.expr) %in% Meta[,from]]
   message(paste0(length(not.exists), " features from data frame not exist in meta file!"))
+  if (output.lost.genes) {
+    cat("Genes exist in RNA-seq data while not exist in scRNAseq data:")
+    cat(not.exists)
+  }
   BulkRNAseq.expr <- BulkRNAseq.expr[rownames(BulkRNAseq.expr) %in%  Meta[,from],]
   mapping <- Meta[,to]
   names(mapping) <- Meta[,from]
@@ -51,8 +57,10 @@ check_RNAseq_df <- function(Adf){
 #' @param Genes.gradient A numeric vector: selected genes numbers for all regions.
 #' @param minimal.RNAseq.value A numeric: remove genes with low expression value in bulk RNA-seq
 #' @param scRNAseq.expression.cut  A numeric: cut off of the scaled value to define whether a gene is expressed or not.
+#' @param Output.lost.genes output the genes exist in RNA-seq data while not exist in scRNA-seq data, Default FALSE.
+#' @param Customized.GeneList A list: customized regional markers
 #'
-#' @return A list: cluster * Region preference for each UMI And Top N genes combination
+#' @return A list: 1. selected genes for each region; 2. cluster * Region preference for each UMI And Top N genes combination
 #' @export
 #'
 #' @examples
@@ -60,6 +68,10 @@ check_RNAseq_df <- function(Adf){
 #' data(RNAseq)
 #' bulkRNAseq <- scRNAseq_Score_Region_Check(RNAseq$EC, FlyGeneMeta)
 #' score.list <- scRNAseq_Score_Region(scRNA, bulkRNAseq)
+#' gene.list <- scRNAseq_Score_Region_ExtractFeatures(score.list)
+#' GeneList <- list()
+#' for(i in colnames(gene.list)){ GeneList[[i]] <- gene.list[,i]}
+#' score.list.customized <- scRNAseq_Score_Region(SeuratObj = scRNA, Customized.GeneList = GeneList)
 scRNAseq_Score_Region <- function(SeuratObj,
                                   BulkRNAseq.expr,
                                   # filter genes by summed UMI count from all clusters, remove genes with low expression in scRNA-seq.
@@ -67,81 +79,104 @@ scRNAseq_Score_Region <- function(SeuratObj,
                                   # select top genes used
                                   Genes.gradient = c(10, 20, 30, 40, 50, 100, 200, 300, 400, 500),
                                   # filter genes by expression value in Bulk RNA-seq
-                                  minimal.RNAseq.value = 0.5,
+                                  minimal.RNAseq.value = 3.5,
                                   # cut off of the scaled value to define if a gene is expressed or not.
-                                  scRNAseq.expression.cut = 0.5
+                                  scRNAseq.expression.cut = 0.5,
+                                  Output.lost.genes = FALSE,
+                                  Customized.GeneList = NULL
 
 
 ){
-  BulkRNAseq.expr <- check_RNAseq_df(BulkRNAseq.expr)
-  # RNAseq里的基因是否都存在于scRNAseq中
-  not.exists <- rownames(BulkRNAseq.expr)[!rownames(BulkRNAseq.expr) %in% rownames(SeuratObj)]
-  message(paste0(length(not.exists), " features from RNA-seq not exist in scRNAseq!"))
-  BulkRNAseq.expr <- BulkRNAseq.expr[rownames(BulkRNAseq.expr) %in% rownames(SeuratObj),]
-  n.region <- ncol(BulkRNAseq.expr) # how many regions
-  sorted.list <- list() # save the ranked fold change results for each region
-  # for each region, choose genes with RPKM value >=3.5, and calculate the fold change compared with other regions.
-  for (i in 1:n.region) {
-    expr_tmp <- BulkRNAseq.expr[BulkRNAseq.expr[,i] >= 3.5,]  # remove genes bellow the minimal expression/RPKM value!
-    expr_tmp <- dplyr::mutate(expr_tmp,fc = expr_tmp[,i]/apply(expr_tmp[,-i],1,function(x)mean(x))) # calculate fold change
-    # results <- dplyr::arrange(expr_tmp,dplyr::desc(fc)) # rank the genes by fold change
-    results <- expr_tmp[order(expr_tmp$fc, decreasing = TRUE),]
-    sorted.list[[colnames(BulkRNAseq.expr)[i]]] <- results
-  }
-  # annotated with summed UMI counts from scRNA-seq data
-  sorted.list <- lapply(sorted.list, function(x){
-    x$UMIs <- apply(SeuratObj@assays$RNA@counts[rownames(x),],1,sum)
-    return(x)
-  })
   # list: cell * gene expression matrix of each cluster, using scaled data(why?)
   cluster.expr.list <- split(data.frame(t(SeuratObj@assays$RNA@scale.data)), as.character(SeuratObj@active.ident))
-  # 计算每个UMI和Top x Genes数目组合下，每个cluster的Region打分
-  main_fun <- function(cluster.expr.list = cluster.expr.list, # cell * gene
-                       sorted.list = sorted.list,
-                       UMI.gradient = UMI.gradient,
-                       Genes.gradient = Genes.gradient){
-    # for each combination of UMI and Top n genes, for each cluster, calculate the region preference.
-    # return a score matrix: Cluster * Region
-    region_cluster_mat <- function(cluster.expr.list = cluster.expr.list, gene.list = gene.list){
-      # build results matrix
-      res.sub <- matrix(0,
-                        nrow = length(cluster.expr.list),
-                        ncol = length(gene.list),
-                        dimnames = list(names(cluster.expr.list),names(gene.list)))
-      for (x in names(cluster.expr.list)) {
-        for (y in names(gene.list)) {
-          expr.sub <- cluster.expr.list[[x]]
-          # remove genes not exist in scRNA-seq
-          genes.region <- gene.list[[y]][gene.list[[y]] %in% colnames(expr.sub)]
-          expr.sub <- expr.sub[,genes.region]
-          # for each region, calculate the enrichment score of each scRNA-seq cluster.
-          expr.sub <- expr.sub > scRNAseq.expression.cut # transfer to a expression binary matrix, 0:not expressed, 1:expressed.
-          cell.percent <- apply(expr.sub, 2, mean) # mean expression percent of each gene
-          res.sub[x,y] <- mean(cell.percent) # mean expression percent of all genes which is normalized by genes numbers, for different gene sets may have different gene numbers
+  # for each region in gene list and for each cluster, calculate the region preference.
+  # return a score matrix: Cluster * Region
+  region_cluster_mat <- function(cluster.expr.list = cluster.expr.list, gene.list = gene.list){
+    # build results matrix
+    res.sub <- matrix(0,
+                      nrow = length(cluster.expr.list),
+                      ncol = length(gene.list),
+                      dimnames = list(names(cluster.expr.list),names(gene.list)))
+    for (x in names(cluster.expr.list)) {
+      for (y in names(gene.list)) {
+        expr.sub <- cluster.expr.list[[x]]
+        # remove genes not exist in scRNA-seq
+        genes.region <- gene.list[[y]][gene.list[[y]] %in% colnames(expr.sub)]
+        expr.sub <- expr.sub[,genes.region]
+        # for each region, calculate the enrichment score of each scRNA-seq cluster.
+        expr.sub <- expr.sub > scRNAseq.expression.cut # transfer to a expression binary matrix, 0:not expressed, 1:expressed.
+        cell.percent <- apply(expr.sub, 2, mean) # mean expression percent of each gene
+        res.sub[x,y] <- mean(cell.percent) # mean expression percent of all genes which is normalized by genes numbers, for different gene sets may have different gene numbers
+      }
+    }
+    return(list(Genes = gene.list, Score = res.sub))
+  }
+
+  # 使用参数过滤的基因list
+  if (is.null(Customized.GeneList)) {
+    BulkRNAseq.expr <- check_RNAseq_df(BulkRNAseq.expr)
+    # RNAseq里的基因是否都存在于scRNAseq中
+    not.exists <- rownames(BulkRNAseq.expr)[!rownames(BulkRNAseq.expr) %in% rownames(SeuratObj)]
+    message(paste0(length(not.exists), " features from RNA-seq not exist in scRNAseq!"))
+    if (Output.lost.genes) {
+      cat("Genes exist in RNA-seq data while not exist in scRNAseq data:")
+      cat(not.exists)
+    }
+    BulkRNAseq.expr <- BulkRNAseq.expr[rownames(BulkRNAseq.expr) %in% rownames(SeuratObj),]
+    n.region <- ncol(BulkRNAseq.expr) # how many regions
+    sorted.list <- list() # save the ranked fold change results for each region
+    # for each region, choose genes with RPKM value >=minimal.RNAseq.value, and calculate the fold change compared with other regions.
+    for (i in 1:n.region) {
+      expr_tmp <- BulkRNAseq.expr[BulkRNAseq.expr[,i] >= minimal.RNAseq.value,]  # remove genes bellow the minimal expression/RPKM value!
+      expr_tmp <- dplyr::mutate(expr_tmp,fc = expr_tmp[,i]/apply(expr_tmp[,-i],1,function(x)mean(x))) # calculate fold change
+      # results <- dplyr::arrange(expr_tmp,dplyr::desc(fc)) # rank the genes by fold change
+      results <- expr_tmp[order(expr_tmp$fc, decreasing = TRUE),]
+      sorted.list[[colnames(BulkRNAseq.expr)[i]]] <- results
+    }
+    # annotated with summed UMI counts from scRNA-seq data
+    sorted.list <- lapply(sorted.list, function(x){
+      x$UMIs <- apply(SeuratObj@assays$RNA@counts[rownames(x),],1,sum)
+      return(x)
+    })
+    # 计算每个UMI和Top x Genes数目组合下，每个cluster的Region打分
+    main_fun <- function(cluster.expr.list = cluster.expr.list, # cell * gene
+                         sorted.list = sorted.list,
+                         UMI.gradient = UMI.gradient,
+                         Genes.gradient = Genes.gradient){
+      # for each combination of UMI and Top n genes, for each cluster, calculate the region preference.
+      # save the final results for each combination
+      res <- list()
+      for (i in as.character(UMI.gradient)) {
+        for (j in as.character(Genes.gradient)) {
+          # for each combination, choose genes with enough UMIs, and only keep the top_n genes
+          gene.list <- lapply(sorted.list, function(x){
+            x <- x[x$UMIs > i,]
+            return(rownames(x)[1:j])
+          })
+          # calculate the region preference of each cluster for each combination
+          res[[i]][[j]] <- region_cluster_mat(cluster.expr.list = cluster.expr.list, gene.list = gene.list)
         }
       }
-      return(res.sub)
+      return(res)
     }
-    # save the final results for each combination
+    # run
+    return(main_fun(cluster.expr.list = cluster.expr.list, # cell * gene
+             sorted.list = sorted.list,
+             UMI.gradient = UMI.gradient,
+             Genes.gradient = Genes.gradient))
+  }else{
+    # Gene list里的基因是否都存在于scRNAseq中
+    not.exists <- unname(unlist(Customized.GeneList))[!unname(unlist(Customized.GeneList)) %in% rownames(SeuratObj)]
+    message(paste0(length(not.exists), " features from RNA-seq not exist in scRNAseq!"))
+    if (Output.lost.genes) {
+      cat("Genes exist in RNA-seq data while not exist in scRNAseq data:")
+      cat(not.exists)
+    }
+    Customized.GeneList <- lapply(Customized.GeneList, function(x){x[x %in% rownames(SeuratObj)]})
     res <- list()
-    for (i in as.character(UMI.gradient)) {
-      for (j in as.character(Genes.gradient)) {
-        # for each combination, choose genes with enough UMIs, and only keep the top_n genes
-        gene.list <- lapply(sorted.list, function(x){
-          x <- x[x$UMIs > i,]
-          return(rownames(x)[1:j])
-        })
-        # calculate the region preference of each cluster for each combination
-        res[[i]][[j]] <- region_cluster_mat(cluster.expr.list = cluster.expr.list, gene.list = gene.list)
-      }
-    }
+    res[["customized"]][["customized"]]<- region_cluster_mat(cluster.expr.list, Customized.GeneList)
     return(res)
   }
-  # run
-  main_fun(cluster.expr.list = cluster.expr.list, # cell * gene
-           sorted.list = sorted.list,
-           UMI.gradient = UMI.gradient,
-           Genes.gradient = Genes.gradient)
 }
 
 #' evaluate the UMI And top n genes combination by Gini index
@@ -161,7 +196,7 @@ scRNAseq_Score_Region <- function(SeuratObj,
 #' score.list <- scRNAseq_Score_Region(scRNA, bulkRNAseq)
 #' scRNAseq_Score_Region_evaluate(score.list)
 scRNAseq_Score_Region_evaluate <- function(ScoreList, ...){
-  gini.list <- lapply(ScoreList, function(x)(unlist(lapply(x, function(x){sum(apply(x, 2, ineq::ineq))}))))
+  gini.list <- lapply(ScoreList, function(x)(unlist(lapply(x, function(x){sum(apply(x$Score, 2, ineq::ineq))}))))
   res.gini <- Reduce(rbind, gini.list)
   rownames(res.gini) <- names(gini.list)
   return(pheatmap::pheatmap(res.gini, ...))
@@ -189,16 +224,53 @@ scRNAseq_Score_Region_evaluate <- function(ScoreList, ...){
 scRNAseq_Score_Region_plot <- function(ScoreList,UMI = NULL, TopGene = NULL, ...){
   if (is.null(UMI) | is.null(TopGene)) {
     # 按Region计算各群表达值的Gini系数，然后对5个Region求和。
-    gini.list <- lapply(ScoreList, function(x)(unlist(lapply(x, function(x){sum(apply(x, 2, ineq::ineq))}))))
+    gini.list <- lapply(ScoreList, function(x)(unlist(lapply(x, function(x){sum(apply(x$Score, 2, ineq::ineq))}))))
     res.gini <- Reduce(rbind, gini.list)
     rownames(res.gini) <- names(gini.list)
     # 有时最大值可能会有多个，用第一个
     umi.gene.max <- which(res.gini == res.gini[which.max(res.gini)], arr.ind=T)[1,]
     message("Using UMI Cutoff: ", rownames(res.gini)[umi.gene.max[1]],"; Genes Used: ", colnames(res.gini)[umi.gene.max[2]])
-    return(pheatmap::pheatmap(ScoreList[[umi.gene.max[1]]][[umi.gene.max[2]]], ...))
+    return(pheatmap::pheatmap(ScoreList[[umi.gene.max[1]]][[umi.gene.max[2]]]$Score, ...))
   }else{
-    return(pheatmap::pheatmap(ScoreList[[as.character(UMI)]][[as.character(TopGene)]], ...))
+    return(pheatmap::pheatmap(ScoreList[[as.character(UMI)]][[as.character(TopGene)]]$Score, ...))
   }
+}
+
+#' Extract features
+#'
+#' @param ScoreList A list: Output from scRNAseq_Score_Region function
+#' @param UMI UMI cutoff defined in scRNAseq_Score_Region function - UMI.gradient
+#' @param TopGene Top n genes defined in scRNAseq_Score_Region function - Genes.gradient
+#'
+#' @return 1. A venn diagram of the genes from different regions; 2.A data frame of the extracted genes for each region.
+#' @export
+#'
+#' @examples
+#' data(scRNA)
+#' data(RNAseq)
+#' bulkRNAseq <- scRNAseq_Score_Region_Check(RNAseq$EC, FlyGeneMeta)
+#' score.list <- scRNAseq_Score_Region(scRNA, bulkRNAseq)
+#' ## futile.logger::flog.threshold(futile.logger::ERROR, name = "VennDiagramLogger")
+#' scRNAseq_Score_Region_ExtractFeatures(score.list)
+#' scRNAseq_Score_Region_ExtractFeatures(score.list, 100, 100)
+scRNAseq_Score_Region_ExtractFeatures <- function(ScoreList,UMI = NULL, TopGene = NULL){
+  if (is.null(UMI) | is.null(TopGene)) {
+    # 按Region计算各群表达值的Gini系数，然后对5个Region求和。
+    gini.list <- lapply(ScoreList, function(x)(unlist(lapply(x, function(x){sum(apply(x$Score, 2, ineq::ineq))}))))
+    res.gini <- Reduce(rbind, gini.list)
+    rownames(res.gini) <- names(gini.list)
+    # 有时最大值可能会有多个，用第一个
+    umi.gene.max <- which(res.gini == res.gini[which.max(res.gini)], arr.ind=T)[1,]
+    UMI <- rownames(res.gini)[umi.gene.max[1]]
+    TopGene <- colnames(res.gini)[umi.gene.max[2]]
+  }
+  message("Using UMI Cutoff: ", UMI,"; Genes Used: ", TopGene)
+  Gene.list <- ScoreList[[as.character(UMI)]][[as.character(TopGene)]]$Genes
+  # futile.logger::flog.threshold(futile.logger::ERROR, name = "VennDiagramLogger")
+  print(grid::grid.draw( VennDiagram::venn.diagram(Gene.list, filename = NULL)))
+  res <- Reduce(cbind, Gene.list)
+  colnames(res) <- names(Gene.list)
+  return(res)
 }
 
 #' plot Euclidean Distance between each combination
@@ -218,7 +290,7 @@ scRNAseq_Score_Region_evaluate2 <- function(ScoreList){
   res.list <- list()
   for (i in names(ScoreList)) {
     for (j in names(ScoreList[[1]])) {
-      res.list[[paste0("UMI-",i,"-Genes-",j)]] <- ScoreList[[i]][[j]]
+      res.list[[paste0("UMI-",i,"-Genes-",j)]] <- ScoreList[[i]][[j]]$Score
     }
   }
   cal_dis <- function(Alist){
@@ -247,6 +319,7 @@ scRNAseq_Score_Region_evaluate2 <- function(ScoreList){
 #' @param Top.foldchange if use selected genes, the cut off of fold change for each Region compared with other regions
 #' @param Top.UMI.Cutoff if use selected genes, the cut off of UMI for each Region
 #' @param Top.numbers if use selected genes, the cut off of top gene numbers for each Region
+#' @param Output.lost.genes output the genes exist in RNA-seq data while not exist in scRNA-seq data, Default FALSE.
 #'
 #' @return A Region * Cluster matrix
 #' @export
@@ -266,12 +339,17 @@ scRNAseq_Score_Region2 <- function(SeuratObj,
                                   Genes.Selection = c("Simple","Top"),
                                   Top.foldchange = 3.5,
                                   Top.UMI.Cutoff = 1,
-                                  Top.numbers = 300
+                                  Top.numbers = 300,
+                                  Output.lost.genes = FALSE
 ){
   BulkRNAseq.expr <- check_RNAseq_df(BulkRNAseq.expr)
   # RNAseq里的基因是否都存在于scRNAseq中
   not.exists <- rownames(BulkRNAseq.expr)[!rownames(BulkRNAseq.expr) %in% rownames(SeuratObj)]
   message(paste0(length(not.exists), " features from RNA-seq not exist in scRNAseq!"))
+  if (Output.lost.genes) {
+    cat("Genes exist in RNA-seq data while not exist in scRNAseq data:")
+    cat(not.exists)
+  }
   BulkRNAseq.expr <- BulkRNAseq.expr[rownames(BulkRNAseq.expr) %in% rownames(SeuratObj),]
   # average expression value for each cluster.
   scRNAseq.averageExpr <- Seurat::AverageExpression(SeuratObj)[[1]]
@@ -333,7 +411,7 @@ scRNAseq_Score_Compare <- function(ScoreList,
   res.list <- list()
   for (i in names(ScoreList)) {
     for (j in names(ScoreList[[1]])) {
-      res.list[[paste0("UMI-",i,"-Genes-",j)]] <- ScoreList[[i]][[j]]
+      res.list[[paste0("UMI-",i,"-Genes-",j)]] <- ScoreList[[i]][[j]]$Score
     }
   }
   cor.res <- c()
--- README.md
+++ README.md
@@ -117,6 +117,29 @@ grid.arrange(p1[[4]],p2[[4]],nrow = 1) & NoLegend()
 
     #> NULL
 
+``` r
+# show the selected genes
+futile.logger::flog.threshold(futile.logger::ERROR, name = "VennDiagramLogger")
+#> NULL
+scRNAseq_Score_Region_ExtractFeatures(score.list)
+#> Using UMI Cutoff: 20; Genes Used: 10
+```
+
+<img src="man/figures/README-unnamed-chunk-7-1.png" width="100%" />
+
+    #> NULL
+    #>       R1        R2                R3         R4         R5       
+    #>  [1,] "CG17738" "Mal-A7"          "Zip42C.2" "Ser8"     "CG17477"
+    #>  [2,] "CG3819"  "CG4830"          "CG6277"   "CG11911"  "CG31265"
+    #>  [3,] "CG6839"  "Npc2f"           "CG43789"  "CG11912"  "CG17475"
+    #>  [4,] "CG43673" "CG8834"          "Vha100-4" "CG17571"  "CG4053" 
+    #>  [5,] "CG11854" "CG15617"         "CG6901"   "Jon99Fi"  "CG6337" 
+    #>  [6,] "to"      "CG9682"          "CG31446"  "CG10472"  "CG32379"
+    #>  [7,] "CG43134" "Peritrophin-15a" "CG31663"  "Jon99Fii" "CG8774" 
+    #>  [8,] "Yp3"     "CG30340"         "CG5770"   "CG7953"   "CG31267"
+    #>  [9,] "CG15649" "CG34040"         "CG30479"  "Jon66Ci"  "CG43208"
+    #> [10,] "Muc68D"  "CG18404"         "CG17930"  "Jon44E"   "CG32284"
+
 **使用自定义参数，即设定UMI Cutoff为100和选取前100个基因。**
 
 ``` r
@@ -125,9 +148,15 @@ p2 <- scRNAseq_Score_Region_plot(score.list, 100, 100, cluster_cols = FALSE, sca
 grid.arrange(p1[[4]],p2[[4]],nrow = 1) & NoLegend()
 ```
 
-<img src="man/figures/README-unnamed-chunk-7-1.png" width="100%" />
+<img src="man/figures/README-unnamed-chunk-8-1.png" width="100%" />
 
     #> NULL
+
+``` r
+# show the selected genes
+# futile.logger::flog.threshold(futile.logger::ERROR, name = "VennDiagramLogger")
+# scRNAseq_Score_Region_ExtractFeatures(score.list, 100, 100)
+```
 
 **可以看到，使用这两种参数，都可以准确判定EC亚群的定位，并可以给出更准确的定位。**
 
@@ -143,7 +172,7 @@ p2 <- pheatmap::pheatmap(score.matrix, scale = "column", cluster_rows = FALSE, s
 grid.arrange(p1[[4]],p2[[4]],nrow = 2) & NoLegend()
 ```
 
-<img src="man/figures/README-unnamed-chunk-8-1.png" width="100%" />
+<img src="man/figures/README-unnamed-chunk-10-1.png" width="100%" />
 
     #> NULL
 
@@ -157,7 +186,7 @@ p2 <- pheatmap::pheatmap(score.matrix, scale = "column", cluster_rows = FALSE, s
 grid.arrange(p1[[4]],p2[[4]],nrow = 2) & NoLegend()
 ```
 
-<img src="man/figures/README-unnamed-chunk-9-1.png" width="100%" />
+<img src="man/figures/README-unnamed-chunk-11-1.png" width="100%" />
 
     #> NULL
 
@@ -185,3 +214,70 @@ head(scRNAseq_Score_Compare(score.list,score.matrix),20)
 ```
 
 **更推荐的方案：从RNAseq的fastq文件入手，计算各个区域的高表达基因，做成基因集合，然后对每一个单细胞进行region定位的偏好性打分（AddModuleScore？），进而也能推断群水平的region偏好性。**
+
+## Session Info
+
+``` r
+sessionInfo()
+#> R version 4.2.1 (2022-06-23 ucrt)
+#> Platform: x86_64-w64-mingw32/x64 (64-bit)
+#> Running under: Windows 10 x64 (build 19043)
+#> 
+#> Matrix products: default
+#> 
+#> locale:
+#> [1] LC_COLLATE=Chinese (Simplified)_China.utf8 
+#> [2] LC_CTYPE=Chinese (Simplified)_China.utf8   
+#> [3] LC_MONETARY=Chinese (Simplified)_China.utf8
+#> [4] LC_NUMERIC=C                               
+#> [5] LC_TIME=Chinese (Simplified)_China.utf8    
+#> 
+#> attached base packages:
+#> [1] stats     graphics  grDevices utils     datasets  methods   base     
+#> 
+#> other attached packages:
+#> [1] MyRPackage_0.0.0.9000 gridExtra_2.3         sp_1.5-0             
+#> [4] SeuratObject_4.1.0    Seurat_4.1.1         
+#> 
+#> loaded via a namespace (and not attached):
+#>   [1] Rtsne_0.16            colorspace_2.0-3      deldir_1.0-6         
+#>   [4] ellipsis_0.3.2        ggridges_0.5.3        futile.logger_1.4.3  
+#>   [7] rstudioapi_0.13       spatstat.data_2.2-0   farver_2.1.0         
+#>  [10] leiden_0.4.2          listenv_0.8.0         ggrepel_0.9.1        
+#>  [13] fansi_1.0.3           codetools_0.2-18      splines_4.2.1        
+#>  [16] knitr_1.39            polyclip_1.10-0       jsonlite_1.8.0       
+#>  [19] ica_1.0-2             cluster_2.1.3         png_0.1-7            
+#>  [22] rgeos_0.5-9           pheatmap_1.0.12       uwot_0.1.11          
+#>  [25] ineq_0.2-13           shiny_1.7.1           sctransform_0.3.3    
+#>  [28] spatstat.sparse_2.1-1 compiler_4.2.1        httr_1.4.3           
+#>  [31] assertthat_0.2.1      Matrix_1.4-1          fastmap_1.1.0        
+#>  [34] lazyeval_0.2.2        cli_3.3.0             formatR_1.12         
+#>  [37] later_1.3.0           htmltools_0.5.2       tools_4.2.1          
+#>  [40] igraph_1.3.2          gtable_0.3.0          glue_1.6.2           
+#>  [43] RANN_2.6.1            reshape2_1.4.4        dplyr_1.0.9          
+#>  [46] Rcpp_1.0.8.3          scattermore_0.8       vctrs_0.4.1          
+#>  [49] nlme_3.1-157          progressr_0.10.1      lmtest_0.9-40        
+#>  [52] spatstat.random_2.2-0 xfun_0.31             stringr_1.4.0        
+#>  [55] globals_0.15.0        mime_0.12             miniUI_0.1.1.1       
+#>  [58] lifecycle_1.0.1       irlba_2.3.5           goftest_1.2-3        
+#>  [61] future_1.26.1         MASS_7.3-57           zoo_1.8-10           
+#>  [64] scales_1.2.0          spatstat.core_2.4-4   promises_1.2.0.1     
+#>  [67] spatstat.utils_2.3-1  parallel_4.2.1        lambda.r_1.2.4       
+#>  [70] RColorBrewer_1.1-3    yaml_2.3.5            reticulate_1.25      
+#>  [73] pbapply_1.5-0         ggplot2_3.3.6         rpart_4.1.16         
+#>  [76] stringi_1.7.6         highr_0.9             rlang_1.0.3          
+#>  [79] pkgconfig_2.0.3       matrixStats_0.62.0    evaluate_0.15        
+#>  [82] lattice_0.20-45       ROCR_1.0-11           purrr_0.3.4          
+#>  [85] tensor_1.5            labeling_0.4.2        patchwork_1.1.1      
+#>  [88] htmlwidgets_1.5.4     cowplot_1.1.1         tidyselect_1.1.2     
+#>  [91] parallelly_1.32.0     RcppAnnoy_0.0.19      plyr_1.8.7           
+#>  [94] magrittr_2.0.3        R6_2.5.1              generics_0.1.2       
+#>  [97] DBI_1.1.3             mgcv_1.8-40           pillar_1.7.0         
+#> [100] fitdistrplus_1.1-8    survival_3.3-1        abind_1.4-5          
+#> [103] tibble_3.1.7          future.apply_1.9.0    crayon_1.5.1         
+#> [106] futile.options_1.0.1  KernSmooth_2.23-20    utf8_1.2.2           
+#> [109] spatstat.geom_2.4-0   plotly_4.10.0         rmarkdown_2.14       
+#> [112] grid_4.2.1            data.table_1.14.2     digest_0.6.29        
+#> [115] VennDiagram_1.7.3     xtable_1.8-4          tidyr_1.2.0          
+#> [118] httpuv_1.6.5          munsell_0.5.0         viridisLite_0.4.0
+```
